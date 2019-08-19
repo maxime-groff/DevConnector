499ee5e88ba251b2058d369946994604f5be2268
Maxime Groff <maxime.groff@gmail.com>
2019-08-19 18:05:46 +0200

    Creating the root redux store

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,30 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import "./App.css";
 import Navbar from "./components/layout/Navbar";
 import Landing from "./components/layout/Landing";
 import Register from "./components/auth/Register";
 import Login from "./components/auth/Login";
+//Redux
+import { Provider } from "react-redux";
+import store from "./store";
+
+import "./App.css";
 
 const App = () => (
-  <Router>
-    <React.Fragment>
-      <Navbar />
-      <Route exact path='/' component={Landing} />
-      <section className='container'>
-        <Switch>
-          <Route exact path='/register' component={Register} />
-          <Route exact path='/login' component={Login} />
-        </Switch>
-      </section>
-    </React.Fragment>
-  </Router>
+  <Provider store={store}>
+    <Router>
+      <React.Fragment>
+        <Navbar />
+        <Route exact path='/' component={Landing} />
+        <section className='container'>
+          <Switch>
+            <Route exact path='/register' component={Register} />
+            <Route exact path='/login' component={Login} />
+          </Switch>
+        </section>
+      </React.Fragment>
+    </Router>
+  </Provider>
 );
 
 export default App;
